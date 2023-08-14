a4a7f5bf1a6b4595cdba376af7cebc71046afe87
Vladyslav <vladsw764@icloud.com>
2023-08-14 14:48:36 +0300

    remove deprecated import from utils.go

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -3,16 +3,12 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
 func ParseBody(r *http.Request, x interface{}) {
-	if body, err := ioutil.ReadAll(r.Body); err == nil {
-		if err := json.Unmarshal(body, x); err != nil {
-			fmt.Println("Error unmarshaling JSON:", err)
-		}
-	} else {
-		fmt.Println("Error reading request body:", err)
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(x); err != nil {
+		fmt.Println("Error decoding JSON:", err)
 	}
 }
